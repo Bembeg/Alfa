0d934110850005f24ff2901f15859801cd963aa3
Bembeg <bembegcz@centrum.cz>
2018-03-08 09:19:51 +0100

    Initial Commit

---
+++ Build/CMakeFiles/A.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/A.dir/A.cc.o"
+  "CMakeFiles/A.dir/src/ADetectorConstruction.cc.o"
+  "CMakeFiles/A.dir/src/ASteppingAction.cc.o"
+  "CMakeFiles/A.dir/src/ARunAction.cc.o"
+  "CMakeFiles/A.dir/src/AActionInitialization.cc.o"
+  "CMakeFiles/A.dir/src/APrimaryGeneratorAction.cc.o"
+  "CMakeFiles/A.dir/src/AEventAction.cc.o"
+  "A.pdb"
+  "A"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
